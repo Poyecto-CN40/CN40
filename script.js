9168cdecb004e1777632f021b0cd7f19d5a0881b
Poyecto-CN40 <ppatri.pt@gmail.com>
2026-08-06 15:12:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+/* ===========================
+   CN40 - Centro de Operaciones
+=========================== */
 
+function activar() {
+
+    const mensaje = document.getElementById("mensaje");
+
+    mensaje.style.display = "block";
+
+    mensaje.scrollIntoView({
+        behavior: "smooth",
+        block: "center"
+    });
+
+}
