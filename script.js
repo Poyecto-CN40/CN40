751767439b2422ca4c5fd01eb95d8aead68e89af
Poyecto-CN40 <ppatri.pt@gmail.com>
2026-08-06 18:17:33 +0200

    Refactor activar function in script.js
    Removed comments and streamlined the activar function.

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-/* ===========================
-   CN40 - Centro de Operaciones
-=========================== */
-
 function activar() {
 
-    const mensaje = document.getElementById("mensaje");
-
-    mensaje.style.display = "block";
-
-    mensaje.scrollIntoView({
-        behavior: "smooth",
-        block: "center"
-    });
+    window.location.href = "menu.html";
 
 }
